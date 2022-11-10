cf0a400ba07d71b2ef6fd897da366b76f548aa4b
tore-statsig <74584483+tore-statsig@users.noreply.github.com>
2022-11-10 11:58:03 -0800

    Add initialize diagnistics, bump minor version (#80)

--- src/StatsigOptions.ts
+++ src/StatsigOptions.ts
@@ -1,4 +1,8 @@
-import { InitCompletionCallback, StatsigEnvironment, StatsigUser } from 'statsig-js';
+import {
+  InitCompletionCallback,
+  StatsigEnvironment,
+  StatsigUser,
+} from 'statsig-js';
 
 // The options for the react SDK and JS SDK have diverged
 // Because initializeValues is not compatible with the StatsigProvider
@@ -17,4 +21,5 @@ export type StatsigOptions = {
   eventLoggingApi?: string;
   prefetchUsers?: StatsigUser[];
   initCompletionCallback?: InitCompletionCallback | null;
+  disableDiagnosticsLogging?: boolean;
 };
